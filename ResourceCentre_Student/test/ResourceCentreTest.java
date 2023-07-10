fb6d2164f1054fa01672d0b0ca005e4fa4c8baec
tebtyj <tebbietyj@gmail.com>
2023-07-10 13:50:20 +0800

    update retriveallchromebook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -128,6 +128,39 @@ public class ResourceCentreTest {
 	public void testRetrieveAllChromebook() {
 		// fail("Not yet implemented");
 		// write your code here
+		// Test Case 1
+		// Test if Item list is not null and empty
+		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
+		assertEquals("Test that the Chromebook arraylist is empty.", 0, chromebookList.size());
+		// Attempt to retrieve the Chromebook
+		String allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, allChromebook);
+
+		// Test Case 2
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		// Test that the list is not empty
+		assertEquals("Test that Chromebook arraylist size is 2.", 2, chromebookList.size());
+		// Attempt to retrieve the Chromebook
+		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n", "CB0011", "My Google Chromebook", "Yes", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n", "CB0012", " SAMSUNG Chromebook", "Yes", "", "Win 10");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allChromebook);
+
+		// Test Case 3
+		cb3.setIsAvailable(false);
+		ResourceCentre.addChromebook(chromebookList, cb3);
+		assertEquals("Test that Chromebook arraylist size is 2.", 3, chromebookList.size());
+		assertFalse("Test that the last item in the arraylist is not available", chromebookList.get(2).getIsAvailable());
+		// Attempt to retrieve the Chromebook
+		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n", "CB0011", "My Google Chromebook", "Yes", "", "Mac OS");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n", "CB0012", " SAMSUNG Chromebook", "Yes", "", "Win 10");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allChromebook);
 	}
 
 	@Test
